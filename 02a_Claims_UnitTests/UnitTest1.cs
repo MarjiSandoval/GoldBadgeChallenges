86c9a49203b62d17465b7568a343416efb1f9459
Marjorie Sandoval <Marjorie.Sandoval74@gmail.com>
2021-11-16 11:35:23 -0500

    Tests added

--- 02a_Claims_UnitTests/UnitTest1.cs
+++ 02a_Claims_UnitTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using _02a_Claims_Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace _02a_Claims_UnitTests
 {
@@ -10,27 +11,56 @@ namespace _02a_Claims_UnitTests
     public class UnitTest1
     {
         private ClaimsRepository _claims;
+        private Claims claim1;
+        private Claims claim2;
+        private Claims claim3;
+
         [TestInitialize]
-        public void init()
+        public void Init()
         {
             _claims = new ClaimsRepository();
-            Claims claim1 = new Claims(1, Claims.ClaimType.Car, "Car accident on 465", 400.00, new DateTime(2018, 04, 25), new DateTime(2018, 04, 27), true);
-            Claims claim2 = new Claims(2, Claims.ClaimType.Home, "House fire in Kitchen", 4000.00, new DateTime(2018, 4, 11), new DateTime(2018, 4, 12), true);
-            Claims claim3 = new Claims(3, Claims.ClaimType.Theft, "stolen pancakes", 4.00, new DateTime(2018, 4, 27), new DateTime(2018, 6, 1), false);
+             claim1 = new Claims(1, Claims.ClaimType.Car, "Car accident on 465", 400.00, new DateTime(2018, 04, 25), new DateTime(2018, 04, 27), true);
+             claim2 = new Claims(2, Claims.ClaimType.Home, "House fire in Kitchen", 4000.00, new DateTime(2018, 4, 11), new DateTime(2018, 4, 12), true);
+             claim3 = new Claims(3, Claims.ClaimType.Theft, "stolen pancakes", 4.00, new DateTime(2018, 4, 27), new DateTime(2018, 6, 1), false);
 
             //need to add the claims to repository after creation
             _claims.NewClaim(claim1);
-            _claims.NewClaim(claim3);
             _claims.NewClaim(claim2);
+            _claims.NewClaim(claim3);
         }
 
         [TestMethod]
         public void CreateNewClaim_ShouldReturnTrue()
         {
             bool expected = true;
-            bool actual = _claims.NewClaim(;
+            bool actual = _claims.NewClaim(claim1);
 
-            Assert.AreEqual(expected, actual)
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetClaims_ShouldReturnTrue()
+        {
+           
+            Queue<Claims> queue = _claims.GetClaims();
+            int expected = 3;
+
+            Assert.AreEqual(expected, queue.Count);
+        }
+
+        [TestMethod]
+        public void SeeWhosNextInLine_ShouldReturnNext()
+        {
+            Claims nextClaim = _claims.SeeWhosNextInLine();
+
+            Claims expected = claim1;
+            
+            Assert.AreEqual(expected, nextClaim);
+        }
+        [TestMethod]
+        public void DeleteExistingClaim_ShouldReturnDeleted()
+        {
+            Assert.IsTrue(_claims.DeleteExistingClaims());
         }
     }
 }
---
+++ 03_BadgesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("03_BadgesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("03_BadgesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eac4be04-aa01-48c2-9aee-6e0ff16dfc3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
